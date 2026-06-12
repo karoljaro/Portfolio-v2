a8184f006ec4d112f571e7376faecafee1fc1051
karoljaro <111643413+karoljaro@users.noreply.github.com>
2026-06-12 16:03:33 +0200

    feat: initialize Nuxt 4 project with Tailwind CSS, add essential files
    - Created nuxt.config.ts for project configuration with Tailwind CSS integration.
    - Added package.json with scripts for build, dev, generate, and preview.
    - Included favicon.ico for branding.
    - Added robots.txt to manage web crawler access.
    - Created tsconfig.json for TypeScript support in the Nuxt application.

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+import tailwindcss from '@tailwindcss/vite';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	compatibilityDate: '2025-07-15',
+	devtools: { enabled: true },
+	future: {
+		compatibilityVersion: 5,
+	},
+	css: ['./app/assets/css/main.css'],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+  pages: true,
+});
