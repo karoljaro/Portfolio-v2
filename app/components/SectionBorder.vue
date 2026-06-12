3a7481731c9f6d31aef7ce9476fbdf81cca4d842
karoljaro <111643413+karoljaro@users.noreply.github.com>
2026-06-12 21:26:47 +0200

    feat: add SectionBorder and SectionLabel components

---
+++ app/components/SectionBorder.vue
@@ -0,0 +1,5 @@
+<template>
+    <section class="border border-solid border-border rounded-xl px-6 py-4">
+        <slot />
+    </section>
+</template>
---
+++ app/components/SectionLabel.vue
@@ -0,0 +1,3 @@
+<template>
+	<h2 class="text-2xl font-medium text-primary">&gt; <slot /></h2>
+</template>
