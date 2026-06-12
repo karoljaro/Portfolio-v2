15dda2a98f858cecf05fe3ef7c6c3bbb81d7a510
karoljaro <111643413+karoljaro@users.noreply.github.com>
2026-06-12 21:26:21 +0200

    feat: update Header component background color to use secondary background color

--- app/components/header/Header.vue
+++ app/components/header/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="relative flex min-h-14 items-center border-b border-border border bg-header px-4 sm:px-6 lg:px-8 rounded-xl">
+    <header class="relative flex min-h-14 items-center border-b border-border border bg-background-secondary px-4 sm:px-6 lg:px-8 rounded-xl">
         <nav aria-label="Language" class="absolute left-4 sm:left-6 lg:left-8">
             <div class="flex items-center gap-1 text-sm font-medium text-muted">
                 <button type="button" class="cursor-pointer transition-colors hover:text-foreground focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-primary">
